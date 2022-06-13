8686dc0cdac28c66e10a5d3b93935dd954234444
MattGuy2 <matthewfrankflammia@gmail.com>
2022-06-13 18:58:04 -0400

    fixing bug in index

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ let hourlyDrain = new cron.CronJob('0 * * * *', async () => {
 				if(pepperoniTag[i].alive == 1 && currentTime.getHours() == 0){
 					//if pepperoni still alive at 12am reward some xp
 					let timeAlive = pepperoniTag[i].startDate - currentTime.valueOf();
-					let timeSeconds = Math.floor(value/1000);
+					let timeSeconds = Math.floor(timeAlive/1000);
 					let timeMins = Math.floor(timeSeconds/60);
 					let timeHours = Math.floor(timeMins/60);
 					let daysAlive = Math.floor(timeHours/24);
