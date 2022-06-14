0e150b02e1441f80d2371e450303b254dd529be4
mattguy <matthewfrankflammia@gmail.com>
2022-06-14 17:37:48 -0400

    storing db function files

---
+++ dbFunctions/change.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
+
+const pepperoni  = require('./model/pepperoni.js')(sequelize, Sequelize.DataTypes);
+remove();
+async function remove(){
+	const tag = await pepperoni.update({gaming:0},{where:{gaming:1}});
+	console.log(tag);
+}
